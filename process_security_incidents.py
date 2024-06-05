d33932c21b146b38cfe639e5f99cc8821aace60b
JacobDubsW <165968109+JacobDubsW@users.noreply.github.com>
2024-06-05 23:58:51 +0000

    Finished

--- process_security_incidents.py
+++ process_security_incidents.py
@@ -5,14 +5,14 @@ output_file = 'security_incidents_modified.csv'
 
 with open(input_file, mode='r') as infile:
     reader = csv.reader(infile)
-    data = list(reader)
+    Security_info = list(reader)
 
 new_column_name = 'Status'
 default_value = 'Pending'
 
-header = data[0] + [new_column_name]
+header = Security_info[0] + [new_column_name]
 
-rows = [row + [default_value] for row in data[1:]]
+rows = [row + [default_value] for row in Security_info[1:]]
 
 with open(output_file, mode='w', newline='') as outfile:
     writer = csv.writer(outfile)
